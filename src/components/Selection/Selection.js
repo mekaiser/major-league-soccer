4f52d9ea49024371a7f9bb8427a01a7ae643e23d
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-06-09 00:14:26 +0600

    removed unnecessary codes

--- src/components/Selection/Selection.js
+++ src/components/Selection/Selection.js
@@ -4,16 +4,6 @@ import SelectionInfo from '../SelectionInfo/SelectionInfo';
 const Selection = (props) => {
     const selectedPlayer = props.selectedPlayer;
     let count = 1;
-    // const totalBudgetGenerator = () =>{
-    //     selectedPlayer.map(each => {
-    //         const newTotalBudget = [...totalBudget, each.salary];
-    //         console.log(newTotalBudget);
-    //         console.log(each.salary);
-    //         setTotalBudget(newTotalBudget);
-    //     })
-    // }
-    // console.log(selectedPlayer[0]);
-    console.log(selectedPlayer);
     const totalBudgetCalculated = selectedPlayer.reduce((totalBudget, each) => totalBudget + Number(each.salary), 0);
     const totalBudgetFinal = totalBudgetCalculated.toFixed(2);
 
